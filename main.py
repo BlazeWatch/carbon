4433fd59eea681569930030976bf45d0f7c28d8c
Tricked <tricked@tricked.pro>
2023-08-08 17:15:22 +0200

    add new tab

--- main.py
+++ main.py
@@ -12,17 +12,20 @@ import sqlalchemy as sa
 import pandas as pd
 
 from dotenv import load_dotenv
+import plotly.graph_objects as go
+import plotly.express as px
 
 load_dotenv()
 
 st.set_page_config(
     page_title="Zaza Monitor",
     layout="wide",
-    page_icon="/app/static/iconee.png"
+    page_icon="/app/static/iconee.png",
+    initial_sidebar_state="collapsed",
 )
 
 
-@st.cache_resource
+@st.cache_resource(ttl=20)
 def get_conn():
     print("Connecting to database")
     engine = sa.create_engine(os.environ.get("DATABASE_URL"))
@@ -55,7 +58,7 @@ def render_template(template: str, **kwargs):
 render_template("css.html")
 
 
-@st.cache_data(ttl=60, show_spinner=False)
+@st.cache_data(ttl=1, show_spinner=False)
 def fetch_objects(query):
     print(f"FO: {query}")
     result_proxy = conn.execute(sa.text(query))
@@ -66,7 +69,6 @@ def fetch_objects(query):
     if records[0]["xy"] is not None:
         for record in records:
             record["xy"] = literal_eval(record["xy"])
-
     return records
 
 
@@ -76,17 +78,85 @@ def fetch_panda_frame(query):
     return pd.read_sql_query(query, conn)
 
 
-while True:
-    day = conn.scalar(sa.text('SELECT MAX(day) FROM public.temp_readings'))
+max_day = conn.scalar(sa.text('SELECT MAX(day) FROM public.temp_readings_production'))
+with st.sidebar:
+    page = st.radio(
+        "What\'s your favorite movie genre",
+        ('realtime', 'stats'))
+    if page == 'realtime':
+        day = st.number_input("Day", min_value=1, max_value=5000, value=max_day)
+        if day > max_day:
+            day = max_day
+        if day == 0:
+            day = max_day
 
-    avg = conn.scalar(sa.text(f'SELECT AVG(temperature) FROM public.temp_readings WHERE day = {day}'))
 
+def calculate_avg_temperature_per_plot():
+    query = 'SELECT xy, AVG(temperature) FROM public.temp_readings_production GROUP BY xy'
+    return fetch_objects(query)
+
+
+def plot_avg_temperature_per_plot():
+    # Fetch average temperature per plot
+    avg_temperatures = calculate_avg_temperature_per_plot()
+
+    # Prepare data for the chart
+    plots = [str(record['xy']) for record in avg_temperatures]
+    temperatures = [record['temperature'] for record in avg_temperatures]
+
+    # Create a DataFrame
+    df_avg_temperatures = pd.DataFrame({
+        'Plot': plots,
+        'Avg Temperature': temperatures
+    })
+
+    # Plot the chart
+    fig = px.bar(df_avg_temperatures, x='Plot', y='Avg Temperature', title='Average Temperature per Plot')
+    st.plotly_chart(fig)
+
+
+if page == 'stats':
+    st.markdown("A small tab with some stats about the dataset")
+
+    tweet_count = conn.scalar(sa.text(f'SELECT COUNT(*) FROM public.tweets_production'))
+    fire_alert_count = conn.scalar(sa.text(f'SELECT COUNT(*) FROM public.firealerts_production'))
+    ai_fire_alert_count = conn.scalar(sa.text(f'SELECT COUNT(*) FROM public.ai_fire_alerts'))
+    temperature_reading_count = conn.scalar(sa.text(f'SELECT COUNT(*) FROM public.temp_readings_production'))
+    avg_temperature = conn.scalar(sa.text(f'SELECT AVG(temperature) FROM public.temp_readings_production'))
+    max_temperature = conn.scalar(sa.text(f'SELECT MAX(temperature) FROM public.temp_readings_production'))
+    min_temperature = conn.scalar(sa.text(f'SELECT MIN(temperature) FROM public.temp_readings_production'))
+
+    st.table([
+        ("Tweet Count", tweet_count),
+        ("Fire Alert Count", fire_alert_count),
+        ("AI Fire Alert Count", ai_fire_alert_count),
+        ("Temperature Reading Count", temperature_reading_count),
+        ("Average Temperature", avg_temperature),
+        ("Max Temperature", max_temperature),
+        ("Min Temperature", min_temperature)
+    ])
+
+    df_stats = pd.DataFrame({
+        "Category": ["Tweet Count", "Fire Alert Count", "AI Fire Alert Count", "Temperature Reading Count"],
+        "Count": [tweet_count, fire_alert_count, ai_fire_alert_count, temperature_reading_count]
+    })
+
+    fig = go.Figure(data=go.Bar(x=df_stats["Category"], y=df_stats["Count"]))
+    fig.update_layout(yaxis_type="log", title_text="Logarithmic Scale Bar Chart")
+    st.plotly_chart(fig)
+
+    plot_avg_temperature_per_plot()
+
+while page != 'stats':
+    avg = conn.scalar(sa.text(f'SELECT AVG(temperature) FROM public.temp_readings_production WHERE day = {day}'))
     with placeholder.container():
         cols = st.columns([1, 3, 1])
 
         with cols[0]:
-            alerts = fetch_objects('SELECT * FROM public.fire_alerts ORDER BY event_day DESC LIMIT 5')
-            ai_alerts = fetch_objects('SELECT * FROM public.ai_fire_alerts ORDER BY day DESC LIMIT 5')
+            alerts = fetch_objects(
+                f'SELECT * FROM public.firealerts_production WHERE notification_day <= {day} ORDER BY event_day DESC LIMIT 5')
+            ai_alerts = fetch_objects(
+                f'SELECT * FROM public.ai_fire_alerts WHERE day <= {day} ORDER  BY day DESC LIMIT 5')
 
             render_template("alerts.jinja2", alerts=alerts, ai_alerts=ai_alerts)
 
@@ -103,12 +173,12 @@ while True:
 
             cols[1].header("Hottest zones")
             cols[1].dataframe(
-                fetch_panda_frame(f"SELECT * FROM temp_readings WHERE day = {day} ORDER BY temperature DESC LIMIT 20"),
+                fetch_panda_frame(
+                    f"SELECT * FROM temp_readings_production WHERE day = {day} ORDER BY temperature DESC LIMIT 20"),
                 use_container_width=True, hide_index=True)
 
         with cols[2]:
-            tweets = fetch_objects(f'SELECT * FROM public.tweets WHERE day = {day} LIMIT 20')
-
-            render_template("tweets.jinja2", tweets=tweets)
+            tweets = fetch_objects(f'SELECT * FROM public.tweets_production WHERE day = {day} LIMIT 20')
+            render_template("tweets.jinja2", tweets=tweets, day=day)
         print("Reloading")
         time.sleep(4)
